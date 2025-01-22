28e0411b3b556b10c0719a5c7352e875345baca5
Matthew Wood <67578755+matt-wood-ct@users.noreply.github.com>
2025-01-22 09:06:54 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -147,7 +147,7 @@ fileUpload.addEventListener('click', event => {
 });
 
 connectButton.addEventListener('click', async () => {
-    let filters = [{ namePrefix: "Fusion Sensor" }];
+    let filters = [{ namePrefix: "Fusion Sensor" }, { namePrefix: "Fusion Wireless Sensor" }];
     await mcumgr.connect(filters);
 });
 
